6e3e9e2b69b34ba2f6bf0cb28a82109f7bee4a6d
wangying000 <1178922618@qq.com>
2016-09-21 10:17:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/leftandrightfragment/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.leftandrightfragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.net.Uri;
+
+import com.example.leftandrightfragment.Words.WordsContent;
+import com.example.leftandrightfragment.DetailFragment;
+
+public class MainActivity extends AppCompatActivity
+        implements ItemFragment.OnListFragmentInteractionListener,
+        DetailFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+
+
+    public void onListFragmentInteraction(WordsContent.WordItem item){
+        Bundle arguments=new Bundle();
+        arguments.putString("id",item.id);
+        Fragment fragment=new DetailFragment();
+        fragment.setArguments(arguments);
+        getFragmentManager().beginTransaction().replace(R.id.worddetail,fragment).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+
+}
